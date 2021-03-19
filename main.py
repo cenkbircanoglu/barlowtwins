71109fa22d787faa2d35fc3ea0ab95f5ad97c2a4
Cenk Bircanoglu <cenk.bircanoglu@adevinta.com>
2021-03-19 14:38:28 +0100

    update gitignore and gpus usage

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+import pytorch_lightning as pl
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torchvision
-import numpy as np
-import pytorch_lightning as pl
-import lightly
-from loss import BarlowTwinsLoss
+from lightly import data as lightly_data, models
+from torch.utils.data import DataLoader
 
-from utils import knn_predict, BenchmarkModule
+from loss import BarlowTwinsLoss
+from utils import BenchmarkModule
 
 num_workers = 8
 max_epochs = 800
@@ -15,16 +14,16 @@ knn_k = 200
 knn_t = 0.1
 classes = 10
 batch_size = 512
-seed=1
+seed = 1
 
 pl.seed_everything(seed)
 
 # use a GPU if available
-gpus = 1 if torch.cuda.is_available() else 0
+gpus = torch.cuda.device_count() if torch.cuda.is_available() else 0
 device = 'cuda' if gpus else 'cpu'
 
 # Use SimCLR augmentations, additionally, disable blur
-collate_fn = lightly.data.SimCLRCollateFunction(
+collate_fn = lightly_data.SimCLRCollateFunction(
     input_size=32,
     gaussian_blur=0.,
 )
@@ -33,28 +32,28 @@ collate_fn = lightly.data.SimCLRCollateFunction(
 test_transforms = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize(
-        mean=lightly.data.collate.imagenet_normalize['mean'],
-        std=lightly.data.collate.imagenet_normalize['std'],
+        mean=lightly_data.collate.imagenet_normalize['mean'],
+        std=lightly_data.collate.imagenet_normalize['std'],
     )
 ])
 
-dataset_train_ssl = lightly.data.LightlyDataset.from_torch_dataset(
+dataset_train_ssl = lightly_data.LightlyDataset.from_torch_dataset(
     torchvision.datasets.CIFAR10(
         root='data',
         train=True,
         download=True))
-dataset_train_kNN = lightly.data.LightlyDataset.from_torch_dataset(torchvision.datasets.CIFAR10(
+dataset_train_kNN = lightly_data.LightlyDataset.from_torch_dataset(torchvision.datasets.CIFAR10(
     root='data',
     train=True,
     transform=test_transforms,
     download=True))
-dataset_test = lightly.data.LightlyDataset.from_torch_dataset(torchvision.datasets.CIFAR10(
+dataset_test = lightly_data.LightlyDataset.from_torch_dataset(torchvision.datasets.CIFAR10(
     root='data',
     train=False,
     transform=test_transforms,
     download=True))
 
-dataloader_train_ssl = torch.utils.data.DataLoader(
+dataloader_train_ssl = DataLoader(
     dataset_train_ssl,
     batch_size=batch_size,
     shuffle=True,
@@ -62,14 +61,14 @@ dataloader_train_ssl = torch.utils.data.DataLoader(
     drop_last=True,
     num_workers=num_workers
 )
-dataloader_train_kNN = torch.utils.data.DataLoader(
+dataloader_train_kNN = DataLoader(
     dataset_train_kNN,
     batch_size=batch_size,
     shuffle=False,
     drop_last=False,
     num_workers=num_workers
 )
-dataloader_test = torch.utils.data.DataLoader(
+dataloader_test = DataLoader(
     dataset_test,
     batch_size=batch_size,
     shuffle=False,
@@ -78,12 +77,11 @@ dataloader_test = torch.utils.data.DataLoader(
 )
 
 
-
 class BartonTwins(BenchmarkModule):
     def __init__(self, dataloader_kNN, gpus, classes, knn_k, knn_t):
         super().__init__(dataloader_kNN, gpus, classes, knn_k, knn_t)
         # create a ResNet backbone and remove the classification head
-        resnet = lightly.models.ResNetGenerator('resnet-18')
+        resnet = models.ResNetGenerator('resnet-18')
         self.backbone = nn.Sequential(
             *list(resnet.children())[:-1],
             nn.AdaptiveAvgPool2d(1),
@@ -91,9 +89,9 @@ class BartonTwins(BenchmarkModule):
         # create a simsiam model based on ResNet
         # note that bartontwins has the same architecture
         self.resnet_simsiam = \
-            lightly.models.SimSiam(self.backbone, num_ftrs=512, num_mlp_layers=3)
+            models.SimSiam(self.backbone, num_ftrs=512, num_mlp_layers=3)
         self.criterion = BarlowTwinsLoss(device=device)
-            
+
     def forward(self, x):
         self.resnet_simsiam(x)
 
@@ -116,7 +114,7 @@ class BartonTwins(BenchmarkModule):
                         optimizer_closure=None,
                         on_tpu=None,
                         using_native_amp=None,
-                        using_lbfgs=None):        
+                        using_lbfgs=None):
         # 120 steps ~ 1 epoch
         if self.trainer.global_step < 1000:
             lr_scale = min(1., float(self.trainer.global_step + 1) / 1000.)
@@ -137,7 +135,7 @@ class BartonTwins(BenchmarkModule):
 model = BartonTwins(dataloader_train_kNN, gpus=gpus, classes=classes, knn_k=knn_k, knn_t=knn_t)
 
 trainer = pl.Trainer(max_epochs=max_epochs, gpus=gpus,
-                    progress_bar_refresh_rate=100)
+                     progress_bar_refresh_rate=100)
 trainer.fit(
     model,
     train_dataloader=dataloader_train_ssl,
